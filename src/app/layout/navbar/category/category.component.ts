bdd992f4be113f9928c05dbfa68f4d72ce65d510
joneilsonlima <joneilson84@gmail.com>
2024-08-11 18:24:40 -0300

    feat: criando componente de categoria

--- src/app/layout/navbar/category/category.component.ts
+++ src/app/layout/navbar/category/category.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { CategoryService } from './service/category.service';
+import { Category } from '../../category.model';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [
+    FontAwesomeModule
+  ],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
-export class CategoryComponent {
+export class CategoryComponent implements OnInit{
+
+  private readonly categoryService = inject(CategoryService);
+
+  public categories: Category[] | undefined;
+  public currentActivateCategory = this.categoryService.getCategoriesByDefault();
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  fetchCategories(): void {
+    this.categories = this.categoryService.getcategories();
+  }
+
+  onChangeCategory(category: Category): void {
+
+  }
 
 }
